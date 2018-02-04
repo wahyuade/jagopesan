4dc55db904d084356493f7387196386c54f51c14
Wahyu Ade Sasongko <wahyuadesasongko@gmail.com>
2018-02-05 05:20:04 +0700

    trying

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const express = require('express')
 const middleware = require('@line/bot-sdk').middleware
 const Client = require('@line/bot-sdk').Client
 
-const app = express()
-const api = express.Router()
-
 const config = {
   channelAccessToken: 'UmCX9fn/s2wC0tQCYHSkvNeSk2LhSJwPPeu0rt2b7K5PUhHoVOH6f/fRM3Y8smyz0MW6zf842sIUXheOh/TX0bfZXNtTI9OLqRenVgVotcaxKtiXFryPw0GGphvH9zRG6viBHVcnk50TdFPLonaEOAdB04t89/1O/w1cDnyilFU=',
   channelSecret: '1545c8836bc3ad1e9d0bd973a95d4c3a'
 }
 
+const app = express()
+const api = express.Router()
+
+const client = new Client(config)
+
 api.post('/webhook', middleware(config), (req, res) => {
-  Client.replyMessage(event.replyToken, {
+  client.replyMessage(event.replyToken, {
     type: 'text',
     text: 'I cannot leave a 1-on-1 chat!',
   })
